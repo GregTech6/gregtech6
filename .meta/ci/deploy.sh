774b0f0d52f30ad58baae694813fcc7a5004b3be
OvermindDL1 <overminddl1@gmail.com>
2018-09-26 11:58:17 -0600

    Add initial CI scripts

---
+++ .meta/ci/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
+PROJECT_PATH="$(dirname "$(dirname "$SCRIPT_PATH")")" # Go up two levels: .meta/ci
+cd "$PROJECT_PATH"
+    
+gradle -g ../.gradle uploadArchives
+
