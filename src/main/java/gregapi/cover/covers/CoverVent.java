22df956bebdd15046fdcc63b3f2e9dacefbf59aa
Gregorius Techneticies <gregoriustech@gmail.com>
2018-09-29 01:13:32 +0200

    ocd fixed some spaces when I looked up something

--- src/main/java/gregapi/cover/covers/CoverVent.java
+++ src/main/java/gregapi/cover/covers/CoverVent.java
@@ -46,9 +46,9 @@ public class CoverVent extends AbstractCoverAttachment {
 		if (aIsServerSide && !aData.mStopped && aData.mTileEntity instanceof IFluidHandler && SERVER_TIME % 320 == 5) {
 			if (WD.collectable_air(aData.mTileEntity.getWorld(), aData.mTileEntity.getOffsetX(aSide), aData.mTileEntity.getOffsetY(aSide), aData.mTileEntity.getOffsetZ(aSide))) {
 				switch(aData.mTileEntity.getWorld().provider.dimensionId) {
-				case  0: UT.Fluids.fill_((IFluidHandler)aData.mTileEntity, ALL_SIDES_THIS_AND_ANY[aSide], FL.Air.make(16000), T); return;
+				case  0: UT.Fluids.fill_((IFluidHandler)aData.mTileEntity, ALL_SIDES_THIS_AND_ANY[aSide], FL.Air       .make(16000), T); return;
 				case -1: UT.Fluids.fill_((IFluidHandler)aData.mTileEntity, ALL_SIDES_THIS_AND_ANY[aSide], FL.Air_Nether.make(16000), T); return;
-				case +1: UT.Fluids.fill_((IFluidHandler)aData.mTileEntity, ALL_SIDES_THIS_AND_ANY[aSide], FL.Air_End.make(16000), T); return;
+				case +1: UT.Fluids.fill_((IFluidHandler)aData.mTileEntity, ALL_SIDES_THIS_AND_ANY[aSide], FL.Air_End   .make(16000), T); return;
 				}
 				String tBiome = aData.mTileEntity.getBiome(aData.mTileEntity.getOffsetX(aSide), aData.mTileEntity.getOffsetZ(aSide)).biomeName;
 				if (BIOMES_SPACE.contains(tBiome)) return;
