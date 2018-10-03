24e928d98ef68a3689f7b12d75e78aec50db33ec
OvermindDL1 <overminddl1@gmail.com>
2018-10-03 14:09:03 -0600

    Set uniform no-daemon and .gradle directory for CI scripts

--- .meta/ci/deploy.sh
+++ .meta/ci/deploy.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 PROJECT_PATH="$(dirname "$(dirname "$SCRIPT_PATH")")" # Go up two levels: .meta/ci
 cd "$PROJECT_PATH"
     
-gradle -g ../.gradle uploadArchives
+gradle --no-daemon -g ../.gradle uploadArchives
 
--- .meta/ci/prebuild.sh
+++ .meta/ci/prebuild.sh
@@ -4,5 +4,5 @@ SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 PROJECT_PATH="$(dirname "$(dirname "$SCRIPT_PATH")")" # Go up two levels: .meta/ci
 cd "$PROJECT_PATH"
 
-gradle -g ../.gradle --refresh-dependencies setupCIWorkspace
+gradle --no-daemon -g ../.gradle --refresh-dependencies setupCIWorkspace
 
