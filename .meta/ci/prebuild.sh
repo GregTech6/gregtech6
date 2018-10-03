3f546e6809942da3c01f23042f4a18b9e39d00e7
OvermindDL1 <overminddl1@gmail.com>
2018-10-03 13:49:32 -0600

    Add prebuild.sh helper script

--- .meta/ci/prebuild.sh
+++ .meta/ci/prebuild.sh
@@ -3,6 +3,6 @@
 SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 PROJECT_PATH="$(dirname "$(dirname "$SCRIPT_PATH")")" # Go up two levels: .meta/ci
 cd "$PROJECT_PATH"
-    
-gradle build
+
+gradle -g ../.gradle --refresh-dependencies setupCIWorkspace
 
