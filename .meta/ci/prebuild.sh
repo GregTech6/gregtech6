301e02774ba9ca2adf35ee9d1f6798ff613c14a9
OvermindDL1 <overminddl1@gmail.com>
2018-10-05 10:45:52 -0600

    Change prebuild to use `build` instead of `dependencies` so not just the POMs are downloaded...

--- .meta/ci/prebuild.sh
+++ .meta/ci/prebuild.sh
@@ -4,5 +4,6 @@ SCRIPT_PATH="$(dirname "$(readlink -f "$0")")"
 PROJECT_PATH="$(dirname "$(dirname "$SCRIPT_PATH")")" # Go up two levels: .meta/ci
 cd "$PROJECT_PATH"
 
-gradle --no-daemon -g ../.gradle --refresh-dependencies setupCIWorkspace dependencies
+# Only `build`ing to get dependencies, the `dependencies` task only grabs the POM's...
+gradle --no-daemon -g ../.gradle --refresh-dependencies setupCIWorkspace build
 
