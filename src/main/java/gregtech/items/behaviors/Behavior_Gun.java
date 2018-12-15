e044929e1e05bebf0ea32a29cc87e4ace0227b65
Gregorius Techneticies <gregoriustech@gmail.com>
2018-12-15 20:03:26 +0100

    attempt at continuing that raytracing thingy, but still a bit too tired...

--- src/main/java/gregtech/items/behaviors/Behavior_Gun.java
+++ src/main/java/gregtech/items/behaviors/Behavior_Gun.java
@@ -23,19 +23,31 @@ import static gregapi.data.CS.*;
 
 import java.util.List;
 
+import gregapi.block.misc.BlockBaseBars;
+import gregapi.block.misc.BlockBaseSpike;
 import gregapi.code.TagData;
 import gregapi.data.CS.SFX;
 import gregapi.data.LH;
+import gregapi.data.OP;
 import gregapi.data.TD;
 import gregapi.item.multiitem.MultiItem;
 import gregapi.item.multiitem.behaviors.IBehavior.AbstractBehaviorDefault;
+import gregapi.oredict.OreDictItemData;
+import gregapi.oredict.OreDictMaterialStack;
+import gregapi.util.OM;
+import gregapi.util.ST;
 import gregapi.util.UT;
 import gregapi.util.WD;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockPane;
+import net.minecraft.block.BlockRail;
+import net.minecraft.block.BlockTorch;
+import net.minecraft.block.material.Material;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.util.Vec3;
@@ -52,22 +64,36 @@ public class Behavior_Gun extends AbstractBehaviorDefault {
 		mBulletType = aBulletType;
 	}
 	
-	public MovingObjectPosition trace(World aWorld, Vec3 aPosA, Vec3 aPosB) {
+	public MovingObjectPosition trace(World aWorld, Vec3 aPosA, Vec3 aPosB, boolean aHitThings) {
 		if (Double.isNaN(aPosA.xCoord) || Double.isNaN(aPosA.yCoord) || Double.isNaN(aPosA.zCoord) || Double.isNaN(aPosB.xCoord) || Double.isNaN(aPosB.yCoord) || Double.isNaN(aPosB.zCoord)) return null;
-		int aAX = UT.Code.roundDown(aPosA.xCoord), aAY = UT.Code.roundDown(aPosA.yCoord), aAZ = UT.Code.roundDown(aPosA.zCoord), aBX = UT.Code.roundDown(aPosB.xCoord), aBY = UT.Code.roundDown(aPosB.yCoord), aBZ = UT.Code.roundDown(aPosB.zCoord);
-		Block aBlock = WD.block(aWorld, aAX, aAY, aAZ);
+		int tCount = 1000, aAX = UT.Code.roundDown(aPosA.xCoord), aAY = UT.Code.roundDown(aPosA.yCoord), aAZ = UT.Code.roundDown(aPosA.zCoord), aBX = UT.Code.roundDown(aPosB.xCoord), aBY = UT.Code.roundDown(aPosB.yCoord), aBZ = UT.Code.roundDown(aPosB.zCoord);
+		byte tSide = SIDE_UNKNOWN;
 		
-		if (aBlock.canCollideCheck(WD.meta(aWorld, aAX, aAY, aAZ), F)) {
-			MovingObjectPosition tPos = aBlock.collisionRayTrace(aWorld, aAX, aAY, aAZ, aPosA, aPosB);
-			if (tPos != null) return tPos;
-		}
-		
-		int tCount = 1000;
 		while (tCount-- >= 0) {
+			Block aBlock = WD.block(aWorld, aAX, aAY, aAZ);
+			byte  aMeta  = WD.meta (aWorld, aAX, aAY, aAZ);
+			
+			if (aBlock.getMaterial() == Material.water || aBlock.getMaterial() == Material.lava) {
+				return new MovingObjectPosition(aAX, aAY, aAZ, tSide, aPosA, T);
+			} else if (aBlock.getMaterial() == Material.glass) {
+				if (aHitThings) {
+					OreDictItemData tData = OM.anydata(ST.make(aBlock, 1, aMeta));
+					for (OreDictMaterialStack tMaterial : tData.getAllMaterialStacks()) if (tMaterial.mAmount > OP.scrapGt.mAmount) {
+						ST.drop(aWorld, aAX+0.5, aAY+0.5, aAZ+0.5, aBlock, tMaterial.mAmount / OP.scrapGt.mAmount, aMeta);
+					}
+				}
+			} else if (aBlock == Blocks.fence || aBlock == Blocks.fence_gate || aBlock == Blocks.web || aBlock == Blocks.mob_spawner || aBlock instanceof BlockPane || aBlock instanceof BlockRail || aBlock instanceof BlockTorch || aBlock instanceof BlockBaseBars || aBlock instanceof BlockBaseSpike || aBlock.getMaterial() == Material.cactus || aBlock.getMaterial() == Material.fire || aBlock.getMaterial() == Material.air || aBlock.getMaterial() == Material.carpet || aBlock.getMaterial() == Material.cloth || aBlock.getMaterial() == Material.leaves || aBlock.getMaterial() == Material.plants || aBlock.getMaterial() == Material.vine) {
+				// Just ignore or assume the Player shot through them.
+			} else if (aBlock.canCollideCheck(aMeta, F)) {
+				MovingObjectPosition tPos = aBlock.collisionRayTrace(aWorld, aAX, aAY, aAZ, aPosA, aPosB);
+				if (tPos != null) return tPos;
+			}
+			
 			if (aAX == aBX && aAY == aBY && aAZ == aBZ) return null;
 			if (Double.isNaN(aPosA.xCoord) || Double.isNaN(aPosA.yCoord) || Double.isNaN(aPosA.zCoord)) return null;
 			
-			byte tSide = SIDE_UNKNOWN;
+			tSide = SIDE_UNKNOWN;
+			
 			double tAX = aAX, tAY = aAY, tAZ = aAZ, tBX = aAX, tBY = aAY, tBZ = aAZ;
 			double tDiffX = aPosB.xCoord - aPosA.xCoord, tDiffY = aPosB.yCoord - aPosA.yCoord, tDiffZ = aPosB.zCoord - aPosA.zCoord;
 			
@@ -95,18 +121,17 @@ public class Behavior_Gun extends AbstractBehaviorDefault {
 			aAX = UT.Code.roundDown(aPosA.xCoord); if (tSide == SIDE_X_POS) aAX--;
 			aAY = UT.Code.roundDown(aPosA.yCoord); if (tSide == SIDE_Y_POS) aAY--;
 			aAZ = UT.Code.roundDown(aPosA.zCoord); if (tSide == SIDE_Z_POS) aAZ--;
-			
-			aBlock = WD.block(aWorld, aAX, aAY, aAZ);
-			
-			if (aBlock.canCollideCheck(WD.meta(aWorld, aAX, aAY, aAZ), F)) {
-				MovingObjectPosition tPos = aBlock.collisionRayTrace(aWorld, aAX, aAY, aAZ, aPosA, aPosB);
-				if (tPos != null) return tPos;
-			}
 		}
 		return null;
 	}
 	
 	public boolean shoot(ItemStack aGun, ItemStack aBullet, EntityPlayer aPlayer) {
+		
+		//MovingObjectPosition tTargetBlock = trace(aPlayer.worldObj, aPosA, aPosB, F);
+		
+		
+		
+		
 		
 		
 		return T;
