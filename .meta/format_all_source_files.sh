7b43488a179634cbb6bb0dc60e12ae1cde48dbef
Gregorius Techneticies <gregoriustech@gmail.com>
2019-07-21 21:50:54 +0200

    Improved optipng Script and ran it too

--- .meta/format_all_source_files.sh
+++ .meta/format_all_source_files.sh
@@ -8,8 +8,8 @@ LICENSE_HEADER="$(cat "$SOURCE_DIR/LICENSE.header")"
 export LICENSE_HEADER
 
 if [ -z "$1" ]; then
-	find "$SOURCE_DIR/../src/main/java" -iname "*.java" -exec "$SOURCE_DIR/format_source_file.sh" {} \;
-#	find "$SOURCE_DIR/../src/main/resources" -iname "*.png" -exec optipng -clobber -silent {} \;
+	find "$SOURCE_DIR/../src/main/java" -mtime -40 -iname "*.java" -exec "$SOURCE_DIR/format_source_file.sh" {} \;
+	find "$SOURCE_DIR/../src/main/resources" -iname "*.png" -mtime -10 -exec optipng -o7 -nc -clobber -silent {} \;
 else
 	for f in "${@}"; do
 	 	"$SOURCE_DIR"/format_source_files.sh "$f"
