ebf53fcf6dc78d28bc6bfd18043ce1468085dd4e
OvermindDL1 <overminddl1@gmail.com>
2020-07-10 18:27:30 -0600

    Remove the broken call in CoFHCore in ASM that causes random crashes on certain worlds in Single Player

--- src/main/java/gregtech/asm/transformers/CoFHCore_CrashFix.java
+++ src/main/java/gregtech/asm/transformers/CoFHCore_CrashFix.java
@@ -3,10 +3,10 @@ package gregtech.asm.transformers;
 import net.minecraft.launchwrapper.IClassTransformer;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
-
-import java.util.Iterator;
 
 public class CoFHCore_CrashFix implements IClassTransformer {
     @Override
@@ -17,9 +17,18 @@ public class CoFHCore_CrashFix implements IClassTransformer {
         ClassReader classReader = new ClassReader(basicClass);
         classReader.accept(classNode, 0);
 
-        for(MethodNode m: classNode.methods) {
-            if(m.name.equals("serverStarting") || m.name.equals("handleIdMappingEvent")) {
-                // TODO:  Look at the bytecode to see what to remove then remove it here
+        outer: for(MethodNode m: classNode.methods) {
+            if(m.name.equals("serverStarting") || m.name.equals("handleIdMappingEvent")) { // Should check `desc` too but no ambiguity here anyway
+                for(int i=0; i<m.instructions.size(); i++) {
+                    AbstractInsnNode insn = m.instructions.get(i);
+                    if(insn instanceof MethodInsnNode) {
+                        MethodInsnNode methcall = (MethodInsnNode)insn;
+                        if(methcall.owner.equals("cofh/core/util/oredict/OreDictionaryArbiter") && methcall.name.equals("initialize") && methcall.desc.equals("()V")) {
+                            m.instructions.remove(methcall);
+                            break outer;
+                        }
+                    }
+                }
             }
         }
 
