733d1e193c634629c54e0b36017e8be44f979740
Gregorius Techneticies <gregoriustech@gmail.com>
2020-12-01 11:25:58 +0100

    Maybe it was just not Wildcard sensitive

--- src/main/java/gregapi/item/multiitem/MultiItemTool.java
+++ src/main/java/gregapi/item/multiitem/MultiItemTool.java
@@ -90,7 +90,7 @@ public class MultiItemTool extends MultiItem implements IItemGTHandTool, IItemGT
 		super(aModID, aUnlocalized);
 		setMaxStackSize(1);
 		
-		if (MD.BG2.mLoaded) try {UT.Reflection.callPublicMethod(Class.forName("mods.battlegear2.api.weapons.WeaponRegistry"), "addTwoHanded", make(W));} catch(Throwable e) {e.printStackTrace(ERR);}
+		if (MD.BG2.mLoaded) try {UT.Reflection.callPublicMethod(Class.forName("mods.battlegear2.api.weapons.WeaponRegistry"), "addTwoHanded", make(0));} catch(Throwable e) {e.printStackTrace(ERR);}
 	}
 	
 	/**
