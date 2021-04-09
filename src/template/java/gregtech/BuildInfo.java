f8cb08b3ae74d51ab76c5403458dc3b3f7000be2
OvermindDL1 <overminddl1@gmail.com>
2021-04-09 16:10:14 -0600

    Getting tired of forcing a recompile every-single-compile just for an unused variable...

--- src/template/java/gregtech/BuildInfo.java
+++ src/template/java/gregtech/BuildInfo.java
@@ -3,6 +3,5 @@ package gregtech;
 public final class BuildInfo {
 	public final static String version = "${version}";
 	public final static String mcversion = "${mcversion}";
-	public final static String builddate = "${builddate}";
 }
 
