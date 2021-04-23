c7488240a96917de8a8e1a3f8ccab7c0440a94ae
Gregorius Techneticies <gregoriustech@gmail.com>
2021-04-23 21:21:43 +0200

    Noticed that I did not credit Zerrens for the Crate Texture

--- .meta/format_all_source_files.sh
+++ .meta/format_all_source_files.sh
@@ -10,6 +10,7 @@ export LICENSE_HEADER
 
 if [ -z "$1" ]; then
 	# The 1am thing takes Gregs Timezone into account.
+	# DO NOT FORGET TO CHANGE LICENSE.header WHEN INCREMENTING YEAR!!!
 	find "$SOURCE_DIR/../src/main/java" -newermt "2021-01-01 01:00:00" -iname "*.java" -exec "$SOURCE_DIR/format_source_file.sh" {} \;
 # 	find "$SOURCE_DIR/../src/main/java" -mtime -40 -iname "*.java" -exec "$SOURCE_DIR/format_source_file.sh" {} \;
 	find "$SOURCE_DIR/../src/main/resources" -iname "*.png" -mtime -10 -exec optipng -o7 -nc -clobber -silent {} \;
