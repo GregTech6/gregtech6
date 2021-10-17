7be7c2b0150c11099e4a74b2f324385db407ae50
Gregorius Techneticies <gregoriustech@gmail.com>
2021-10-17 15:19:19 +0200

    autogenerate generifier recipes for nonstandard fluids

--- src/main/java/gregtech/loaders/c/Loader_Recipes_Temporary.java
+++ src/main/java/gregtech/loaders/c/Loader_Recipes_Temporary.java
@@ -23,6 +23,8 @@ import static gregapi.data.CS.*;
 import static gregapi.data.OP.*;
 import static gregapi.util.CR.*;
 
+import java.util.Map.Entry;
+
 import gregapi.data.ANY;
 import gregapi.data.CS.BlocksGT;
 import gregapi.data.CS.FluidsGT;
@@ -628,5 +630,7 @@ public class Loader_Recipes_Temporary implements Runnable {
 		RM.generify   (FL.Oil_Nut                 .make( 1), FL.Oil_Seed.make(1));
 		
 		for (String tFluid : FluidsGT.JUICE) if (FL.exists(tFluid)) RM.generify(FL.make(tFluid, 1), FL.Juice.make(1));
+		
+		for (Entry<String, String> tEntry : FluidsGT.FLUID_RENAMINGS.entrySet()) RM.generify(FL.make(tEntry.getKey(), 1), FL.make(tEntry.getValue(), 1));
 	}
 }
