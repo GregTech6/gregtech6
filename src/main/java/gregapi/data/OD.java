cab8fd93db6d39fee7dd3770a47de7274f35d414
Gregorius Techneticies <gregorius.techneticies@protonmail.ch>
2023-05-04 14:01:14 +0200

    More Recipe fixes

--- src/main/java/gregapi/data/OD.java
+++ src/main/java/gregapi/data/OD.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2022 GregTech-6 Team
+ * Copyright (c) 2023 GregTech-6 Team
  *
  * This file is part of GregTech.
  *
@@ -175,6 +175,7 @@ public enum OD {
 	, itemLubricantEarly
 	, itemResin
 	, itemRubber
+	, itemInsulator
 	, itemSalt
 	, itemRock
 	, itemFlint
