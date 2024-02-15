634035b217b8e272237b1636b8260b842e226146
Gregorius Techneticies <gregorius.techneticies@protonmail.ch>
2024-02-15 14:45:34 +0100

    update to current year

--- .meta/format_all_source_files.sh
+++ .meta/format_all_source_files.sh
@@ -12,7 +12,7 @@ if [ -z "$1" ]; then
 	# If there is a different Date mentioned than the 01 01, then that just means Greg might've gotten a new SSD or Computer.
 	# The 1am thing takes Gregs Timezone into account.
 	# DO NOT FORGET TO CHANGE LICENSE.header WHEN INCREMENTING YEAR!!!
-	find "$SOURCE_DIR/../src/main/java" -newermt "2023-01-01 01:00:00" -iname "*.java" -exec chmod 664 {} \; -exec "$SOURCE_DIR/format_source_file.sh" {} \;
+	find "$SOURCE_DIR/../src/main/java" -newermt "2024-01-01 01:00:00" -iname "*.java" -exec chmod 664 {} \; -exec "$SOURCE_DIR/format_source_file.sh" {} \;
 # 	find "$SOURCE_DIR/../src/main/java" -mtime -40 -iname "*.java" -exec "$SOURCE_DIR/format_source_file.sh" {} \;
 	find "$SOURCE_DIR/../src/main/resources" -iname "*.sh"  -mtime -10 -exec chmod 774 {} \;
 	find "$SOURCE_DIR/../src/main/resources" -iname "*.png" -mtime -10 -exec chmod 664 {} \; -exec optipng -o7 -nc -clobber -silent {} \;
