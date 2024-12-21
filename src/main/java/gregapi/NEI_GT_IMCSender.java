d567124339286282e46da847a84f69d7d19d7ee3
kuzuanpa <misaka16384@outlook.com>
2024-12-22 04:24:17 +0800

    resort NEI machines

--- src/main/java/gregapi/NEI_GT_IMCSender.java
+++ src/main/java/gregapi/NEI_GT_IMCSender.java
@@ -1,70 +1,87 @@
 package gregapi;
 
 import cpw.mods.fml.common.event.FMLInterModComms;
+import gregapi.block.multitileentity.MultiTileEntityRegistry;
 import gregapi.data.MD;
+import gregapi.oredict.OreDictMaterial;
 import gregapi.recipes.Recipe;
 import gregapi.util.ST;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static gregapi.data.CS.*;
 
 public class NEI_GT_IMCSender implements Runnable{
     @Override
     public void run() {
         for (Recipe.RecipeMap tMap : Recipe.RecipeMap.RECIPE_MAP_LIST) if (tMap.mNEIAllowed) {
-            String stackName = ST.regMeta(tMap.mRecipeMachineList.isEmpty() ? ST.make(Blocks.lit_furnace, 1, 0) : tMap.mRecipeMachineList.get(0));
-            sendHandler(tMap.mNameNEI,stackName);
+            sortMachines(tMap.mRecipeMachineList);
 
-            //sortMachines(tMap.mRecipeMachineList);
+            String stackName = ST.regMeta(tMap.mRecipeMachineList.isEmpty() ? ST.make(Blocks.lit_furnace, 1, 0) : tMap.mRecipeMachineList.get(0));
+            sendHandler(tMap.mNameNEI,getLeastPriority(stackName));
+
             for (ItemStack aStack : tMap.mRecipeMachineList) if (ST.valid(aStack)) {
-                sendCatalyst(tMap.mNameNEI,ST.regMeta(aStack),0); //getSortedPriority(aStack));
+                sendCatalyst(tMap.mNameNEI,ST.regMeta(aStack) ,getSortedPriority(aStack));
             }
         };
-        //mCatalystPriority = null;
+        mCatalystPriority = null;
     }
-    //Seems unnecessary...
-    //protected @Nullable HashMap<String, Integer> mCatalystPriority = new HashMap<>();
-    //
-    //public void sortMachines(List<ItemStack> stackList){
-    //    mCatalystPriority = new HashMap<>();
-    //    HashMap<String, Double> priorityTmp = new HashMap<>();
-    //    for (ItemStack stack : stackList) {
-    //        if(!(stack.hasTagCompound())) {
-    //            mCatalystPriority.put(ST.regMeta(stack), 0);
-    //            continue;
-    //        }
-    //        NBTTagCompound tag = stack.getTagCompound();
-    //        if(tag.hasKey(NBT_INPUT_MAX))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_INPUT_MAX));
-    //        else if(tag.hasKey(NBT_INPUT))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_INPUT));
-    //        else if(tag.hasKey(NBT_OUTPUT_MAX))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_OUTPUT_MAX));
-    //        else if(tag.hasKey(NBT_OUTPUT))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_OUTPUT));
-    //        else if(tag.hasKey(NBT_MATERIAL))priorityTmp.put(ST.regMeta(stack),OreDictMaterial.get(tag.getString(NBT_MATERIAL)).mMeltingPoint * 1.25);
-    //        else priorityTmp.put(ST.regMeta(stack),0.0);
-    //    }
-    //    mCatalystPriority = sortMap(priorityTmp);
-    //}
-    //public static HashMap<String, Integer> sortMap(HashMap<String, Double> originalMap) {
-    //    //sort them based on the double values
-    //    List<Map.Entry<String, Double>> entryList = new ArrayList<>(originalMap.entrySet());
-    //    entryList.sort(Map.Entry.comparingByValue());
-    //
-    //    // Step 2: Create a new HashMap to store the sorted indices
-    //    HashMap<String, Integer> sortedIndices = new HashMap<>();
-    //
-    //    // Step 3: Assign indices based on the sorted order
-    //    int index = 0;
-    //    for (Map.Entry<String, Double> entry : entryList) {
-    //        sortedIndices.put(entry.getKey(), index++);
-    //    }
-    //
-    //    return sortedIndices;
-    //}
-    //public int getSortedPriority(ItemStack stack){
-    //    if(mCatalystPriority==null)return 0;
-    //    Integer i = mCatalystPriority.get(ST.regMeta(stack));
-    //    if(i == null)return 0;
-    //    return i;
-    //}
+
+    protected @Nullable HashMap<String, Integer> mCatalystPriority = new HashMap<>();
+
+    public void sortMachines(List<ItemStack> stackList){
+        mCatalystPriority = new HashMap<>();
+        HashMap<String, Double> priorityTmp = new HashMap<>();
+        for (ItemStack stack : stackList) {
+            MultiTileEntityRegistry reg = MultiTileEntityRegistry.getRegistry(ST.id(stack));
+            if(reg == null || reg.getClassContainer(stack) == null) continue;
+            NBTTagCompound tag = reg.getClassContainer(stack).mParameters;
+            if(tag == null)continue;
+
+            if(tag.hasKey(NBT_INPUT_MAX))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_INPUT_MAX));
+            else if(tag.hasKey(NBT_INPUT))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_INPUT));
+            else if(tag.hasKey(NBT_OUTPUT_MAX))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_OUTPUT_MAX));
+            else if(tag.hasKey(NBT_OUTPUT))priorityTmp.put(ST.regMeta(stack), (double) tag.getLong(NBT_OUTPUT));
+            else if(tag.hasKey(NBT_MATERIAL))priorityTmp.put(ST.regMeta(stack), OreDictMaterial.get(tag.getString(NBT_MATERIAL)).mMeltingPoint * 1.25);
+        }
+        mCatalystPriority = sortMap(priorityTmp);
+    }
+    public static HashMap<String, Integer> sortMap(HashMap<String, Double> originalMap) {
+        List<Map.Entry<String, Double>> entryList = new ArrayList<>(originalMap.entrySet());
+        entryList.sort(Map.Entry.comparingByValue());
+
+        HashMap<String, Integer> sortedIndices = new HashMap<>();
+
+        int index = entryList.size();
+        for (Map.Entry<String, Double> entry : entryList) {
+            sortedIndices.put(entry.getKey(), index--);
+        }
+
+        return sortedIndices;
+    }
+
+    public String getLeastPriority(String orElse){
+        if(mCatalystPriority==null||mCatalystPriority.isEmpty())return orElse;
+        for (Map.Entry<String, Integer> entry : mCatalystPriority.entrySet()) {
+            if(entry.getValue() == mCatalystPriority.size())return entry.getKey();
+        }
+        return orElse;
+    }
+
+    public int getSortedPriority(ItemStack stack){
+        if(mCatalystPriority==null)return 0;
+        Integer i = mCatalystPriority.get(ST.regMeta(stack));
+        if(i == null)return 0;
+        return i;
+    }
+
 
     private static void sendHandler(String aName, String aBlock) {
         NBTTagCompound aNBT = new NBTTagCompound();
