7bf1f13b96e3a779ee12bf5dde1d8b7ab8382572
Gregorius Techneticies <gregorius.techneticies@protonmail.ch>
2025-04-18 01:48:02 +0200

    increased size of mob spawner area in gt6 dungeons

--- src/main/java/gregapi/worldgen/dungeon/DungeonChunkRoomFarmMobs.java
+++ src/main/java/gregapi/worldgen/dungeon/DungeonChunkRoomFarmMobs.java
@@ -38,89 +38,54 @@ public class DungeonChunkRoomFarmMobs extends DungeonChunkRoomEmpty {
 		aData.mTags.add(WorldgenDungeonGT.TAG_FARM_MOBS);
 		
 		// Check if we have space for a large Setup or not.
-		boolean tSpaceous = 
+		boolean tXNZN = 
 		(aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ-1] == 0 || aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ-1] == -128) &&
 		(aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ  ] == 0 || aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ  ] == -128) &&
-		(aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ+1] == 0 || aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ+1] == -128) &&
-		(aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ-1] == 0 || aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ-1] == -128) &&
-		(aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ+1] == 0 || aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ+1] == -128) &&
+		(aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ-1] == 0 || aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ-1] == -128);
+		boolean tXPZN = 
 		(aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ-1] == 0 || aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ-1] == -128) &&
 		(aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ  ] == 0 || aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ  ] == -128) &&
-		(aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ+1] == 0 || aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ+1] == -128);
-		
-		if (tSpaceous && F) {// TODO remove this flag
-			// Roof, two Blocks thick due to Sunlight glitches.
-			for (int tX = -16; tX < 32; tX++) for (int tZ = -16; tZ < 32; tZ++) {
-				aData.tiles     (tX, 43, tZ);
-				aData.smalltiles(tX, 42, tZ);
-			}
-			
-			// Outer Walls.
-			for (int tY = 9; tY < 42; tY++) {
-				for (int tX = -16; tX < 32; tX++) for (int tZ = -16; tZ < 32; tZ++) {
-					if (tX == -16 || tX == 31 || tZ == -16 || tZ == 31) {
-						aData.bricks(tX, tY, tZ);
-					} else {
-						aData.air(tX, tY, tZ);
-					}
-				}
-			}
-		} else {
-			// Roof, two Blocks thick due to Sunlight glitches.
-			for (int tX = 0; tX < 16; tX++) for (int tZ = 0; tZ < 16; tZ++) {
-				aData.tiles     (tX, 43, tZ);
-				aData.smalltiles(tX, 42, tZ);
-			}
-			
-			// Outer Walls.
-			for (int tY = 9; tY < 42; tY++) {
-				for (int tX = 0; tX < 16; tX++) for (int tZ = 0; tZ < 16; tZ++) {
-					if (tX == 0 || tX == 15 || tZ == 0 || tZ == 15) {
-						aData.bricks(tX, tY, tZ);
-					} else {
-						aData.air(tX, tY, tZ);
-					}
-				}
-			}
-		}
+		(aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ-1] == 0 || aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ-1] == -128);
+		boolean tXNZP = 
+		(aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ+1] == 0 || aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ+1] == -128) &&
+		(aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ  ] == 0 || aData.mRoomLayout[aData.mRoomX-1][aData.mRoomZ  ] == -128) &&
+		(aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ+1] == 0 || aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ+1] == -128);
+		boolean tXPZP = 
+		(aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ+1] == 0 || aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ+1] == -128) &&
+		(aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ  ] == 0 || aData.mRoomLayout[aData.mRoomX+1][aData.mRoomZ  ] == -128) &&
+		(aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ+1] == 0 || aData.mRoomLayout[aData.mRoomX  ][aData.mRoomZ+1] == -128);
 		
 		// Make a solid Pillar, the rest will be carved away when needed.
 		for (int tY =  1; tY <=  6; tY++) for (int tX =  6; tX <=  9; tX++) for (int tZ =  6; tZ <=  9; tZ++) aData.bricks(tX, tY, tZ);
 		for (int tY =  7; tY <=  8; tY++) for (int tX =  5; tX <= 10; tX++) for (int tZ =  5; tZ <= 10; tZ++) aData.smalltiles(tX, tY, tZ);
 		
-		// Blue or Red Steel Omni-Spikes, so all possible Mobs are taken care of!
-		int tSpike = (aData.next1in2() ? 6 : 14);
-		aData.set     ( 7,  9,  7, BlocksGT.Spikes_Steel, tSpike);
-		aData.set     ( 7,  9,  8, BlocksGT.Spikes_Steel, tSpike);
-		aData.set     ( 8,  9,  7, BlocksGT.Spikes_Steel, tSpike);
-		aData.set     ( 8,  9,  8, BlocksGT.Spikes_Steel, tSpike);
-		
-		// Steel Hoppers!
-		aData.set     ( 7,  8,  7, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_X_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 7,  8,  8, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_X_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 8,  8,  7, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_Z_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 8,  8,  8, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_Y_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		// Platforms for Mobs to spawn on.
+		makePlatForms(aData, 0, 0);
+		if (tXNZN || tXNZP) makePlatForms(aData,-16,  0);
+		if (tXPZN || tXPZP) makePlatForms(aData,+16,  0);
+		if (tXNZN || tXPZN) makePlatForms(aData,  0,-16);
+		if (tXNZP || tXPZP) makePlatForms(aData,  0,+16);
+		if (tXNZN) makePlatForms(aData,-16,-16);
+		if (tXPZN) makePlatForms(aData,+16,-16);
+		if (tXNZP) makePlatForms(aData,-16,+16);
+		if (tXPZP) makePlatForms(aData,+16,+16);
 		
 		// Item Pipes and Mass Storages.
-		aData.chiseled( 7,  7,  7);
-		aData.chiseled( 7,  7,  8);
-		aData.chiseled( 8,  7,  7);
-		aData.set     ( 8,  7,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 8,  6,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 8,  5,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 8,  4,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		
+		aData.set     ( 8,  7,  8, SIDE_DOWN   , 25377, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 8,  6,  8, SIDE_DOWN   , 25377, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 8,  5,  8, SIDE_DOWN   , 25377, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 8,  4,  8, SIDE_DOWN   , 25377, UT.NBT.make(NBT_CONNECTION    ,  3, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
 		aData.chiseled( 6,  3,  6);
 		aData.set     ( 6,  3,  7, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_X_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.glass_bottle  , 1+aData.next(8), 0))), T, T);
 		aData.set     ( 6,  3,  8, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_X_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.slime_ball    , 1+aData.next(8), 0))), T, T);
 		aData.chiseled( 6,  3,  9);
 		aData.set     ( 7,  3,  6, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_Z_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.string        , 1+aData.next(8), 0))), T, T);
-		aData.set     ( 7,  3,  7, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 7,  3,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 7,  3,  7, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 7,  3,  8, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
 		aData.set     ( 7,  3,  9, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_Z_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.redstone      , 1+aData.next(8), 0))), T, T);
 		aData.set     ( 8,  3,  6, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_Z_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.spider_eye    , 1+aData.next(8), 0))), T, T);
-		aData.set     ( 8,  3,  7, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 8,  3,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    , 63, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 8,  3,  7, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 8,  3,  8, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 63, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
 		aData.set     ( 8,  3,  9, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_Z_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.glowstone_dust, 1+aData.next(8), 0))), T, T);
 		aData.chiseled( 9,  3,  6);
 		aData.set     ( 9,  3,  7, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_X_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.bone          , 1+aData.next(8), 0))), T, T);
@@ -132,51 +97,111 @@ public class DungeonChunkRoomFarmMobs extends DungeonChunkRoomEmpty {
 		aData.set     ( 6,  2,  8, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_X_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.feather       , 1+aData.next(8), 0))), T, T);
 		aData.chiseled( 6,  2,  9);
 		aData.set     ( 7,  2,  6, SIDE_UNKNOWN,  4009, UT.NBT.make(NBT_FACING, SIDE_Z_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 7,  2,  7, SIDE_UNKNOWN, 25005, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 7,  2,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 7,  2,  7, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 7,  2,  8, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
 		aData.set     ( 7,  2,  9, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_Z_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.gunpowder     , 1+aData.next(8), 0))), T, T);
 		aData.set     ( 8,  2,  6, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_Z_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.rotten_flesh  , 1+aData.next(8), 0))), T, T);
-		aData.set     ( 8,  2,  7, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
-		aData.set     ( 8,  2,  8, SIDE_UNKNOWN, 25002, UT.NBT.make(NBT_CONNECTION    , 62, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 8,  2,  7, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 60, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     ( 8,  2,  8, SIDE_UNKNOWN, 25377, UT.NBT.make(NBT_CONNECTION    , 62, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
 		aData.set     ( 8,  2,  9, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_Z_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.sugar         , 1+aData.next(8), 0))), T, T);
 		aData.chiseled( 9,  2,  6);
 		aData.set     ( 9,  2,  7, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_X_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(ST.make(Items.arrow         , 1+aData.next(8), 0))), T, T);
 		aData.set     ( 9,  2,  8, SIDE_UNKNOWN,  6009, UT.NBT.make(NBT_FACING, SIDE_X_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T, NBT_INV_LIST, UT.NBT.makeInv(OP.arrowGtWood.mat(MT.Empty , 1+aData.next(8)   ))), T, T);
 		aData.chiseled( 9,  2,  9);
 		
-		// Water Placement in center Area.
-		aData.smooth( 1,  9,  1); aData.smooth( 2,  9,  1); aData.smooth( 3,  9,  1); aData.smooth( 4,  9,  1);
-		aData.smooth( 1,  9,  2); aData.smooth( 2,  9,  2); aData.smooth( 3,  9,  2);
-		aData.smooth( 1,  9,  3); aData.smooth( 2,  9,  3);
-		aData.smooth( 1,  9,  4);
-		aData.smooth(14,  9,  1); aData.smooth(13,  9,  1); aData.smooth(12,  9,  1); aData.smooth(11,  9,  1);
-		aData.smooth(14,  9,  2); aData.smooth(13,  9,  2); aData.smooth(12,  9,  2);
-		aData.smooth(14,  9,  3); aData.smooth(13,  9,  3);
-		aData.smooth(14,  9,  4);
-		aData.smooth( 1,  9, 14); aData.smooth( 2,  9, 14); aData.smooth( 3,  9, 14); aData.smooth( 4,  9, 14);
-		aData.smooth( 1,  9, 13); aData.smooth( 2,  9, 13); aData.smooth( 3,  9, 13);
-		aData.smooth( 1,  9, 12); aData.smooth( 2,  9, 12);
-		aData.smooth( 1,  9, 11);
-		aData.smooth(14,  9, 14); aData.smooth(13,  9, 14); aData.smooth(12,  9, 14); aData.smooth(11,  9, 14);
-		aData.smooth(14,  9, 13); aData.smooth(13,  9, 13); aData.smooth(12,  9, 13);
-		aData.smooth(14,  9, 12); aData.smooth(13,  9, 12);
-		aData.smooth(14,  9, 11);
-		aData.set( 1, 10,  1, Blocks.flowing_water, 0, 3);
-		aData.set( 1, 10, 14, Blocks.flowing_water, 0, 3);
-		aData.set(14, 10,  1, Blocks.flowing_water, 0, 3);
-		aData.set(14, 10, 14, Blocks.flowing_water, 0, 3);
-		
-		// Platforms for Mobs to spawn on in center Area.
-		final int[] tPlatforms = { 3, 4, 5, 6, 9,10,11,12};
-		for (int tY = 12; tY < 42; tY++) if (tY % 3 == 0) {
-			for (int i : tPlatforms) for (int j : tPlatforms) aData.cobbles( i, tY,  j);
-			for (int i : tPlatforms) {
-				aData.set( i, tY,  2, Blocks.trapdoor, 12); aData.set( i, tY,  8, Blocks.trapdoor, 12);
-				aData.set( i, tY, 13, Blocks.trapdoor, 13); aData.set( i, tY,  7, Blocks.trapdoor, 13);
-				aData.set( 2, tY,  i, Blocks.trapdoor, 14); aData.set( 8, tY,  i, Blocks.trapdoor, 14);
-				aData.set(13, tY,  i, Blocks.trapdoor, 15); aData.set( 7, tY,  i, Blocks.trapdoor, 15);
+		return T;
+	}
+	
+	public static boolean makePlatForms(DungeonData aData, int aX, int aZ) {
+		for (int eX = aX+15, tX = aX; tX <= eX; tX++)
+		for (int eZ = aZ+15, tZ = aZ; tZ <= eZ; tZ++) {
+			// Roof, two Blocks thick due to Sunlight glitches.
+			aData.tiles     (tX, 43, tZ);
+			aData.smalltiles(tX, 42, tZ);
+			// Floor for the Water to flow on.
+			aData.smalltiles(tX,  8, tZ);
+			// Outer Walls filled with Air.
+			for (int tY = 9; tY < 42; tY++) {
+				if (tX == aX || tX == eX || tZ == aZ || tZ == eZ) {
+					aData.bricks(tX, tY, tZ);
+				} else {
+					aData.air(tX, tY, tZ);
+				}
 			}
 		}
+		
+		// Platforms for Mobs to spawn on.
+		final int[] tPlatforms = { 3, 4, 5, 6, 9,10,11,12};
+		for (int tY = 12; tY < 42; tY++) if (tY % 3 == 0) {
+			for (int i : tPlatforms) for (int j : tPlatforms) aData.cobbles(aX+ i, tY, aZ+ j);
+			for (int i : tPlatforms) {
+				aData.set(aX+ i, tY, aZ+ 2, Blocks.trapdoor, 12); aData.set(aX+ i, tY, aZ+ 8, Blocks.trapdoor, 12);
+				aData.set(aX+ i, tY, aZ+13, Blocks.trapdoor, 13); aData.set(aX+ i, tY, aZ+ 7, Blocks.trapdoor, 13);
+				aData.set(aX+ 2, tY, aZ+ i, Blocks.trapdoor, 14); aData.set(aX+ 8, tY, aZ+ i, Blocks.trapdoor, 14);
+				aData.set(aX+13, tY, aZ+ i, Blocks.trapdoor, 15); aData.set(aX+ 7, tY, aZ+ i, Blocks.trapdoor, 15);
+			}
+		}
+		
+		// Blue or Red Steel Omni-Spikes, so all possible Mobs are taken care of!
+		int tSpike = (aData.next1in2() ? 6 : 14);
+		aData.set     (aX+ 7,  9, aZ+ 7, BlocksGT.Spikes_Steel, tSpike);
+		aData.set     (aX+ 7,  9, aZ+ 8, BlocksGT.Spikes_Steel, tSpike);
+		aData.set     (aX+ 8,  9, aZ+ 7, BlocksGT.Spikes_Steel, tSpike);
+		aData.set     (aX+ 8,  9, aZ+ 8, BlocksGT.Spikes_Steel, tSpike);
+		
+		// Steel Hoppers!
+		aData.set     (aX+ 7,  8, aZ+ 7, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_X_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     (aX+ 7,  8, aZ+ 8, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_X_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     (aX+ 8,  8, aZ+ 7, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_Z_POS, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		aData.set     (aX+ 8,  8, aZ+ 8, SIDE_UNKNOWN,  8010, UT.NBT.make(NBT_FACING, SIDE_Y_NEG, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		
+		// Item Pipes.
+		aData.chiseled(aX+ 7,  7, aZ+ 7);
+		aData.chiseled(aX+ 7,  7, aZ+ 8);
+		aData.chiseled(aX+ 8,  7, aZ+ 7);
+		aData.set     (aX+ 8,  7, aZ+ 8, SIDE_Y_NEG, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_U, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		if (aX > 0) {
+		aData.set     (aX+ 7,  7, aZ+ 8, SIDE_X_POS, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_W | SBIT_E, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		for (int tX = aX- 7, eX = aX+ 6; tX <= eX; tX++)
+		aData.set     (tX   ,  7, aZ+ 8, SIDE_X_NEG, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_W | SBIT_E, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		}
+		if (aX < 0) {
+		aData.set     (aX+ 9,  7, aZ+ 8, SIDE_X_NEG, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_W | SBIT_E, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		for (int tX = aX+10, eX = aX+23; tX <= eX; tX++)
+		aData.set     (tX   ,  7, aZ+ 8, SIDE_X_POS, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_W | SBIT_E, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		}
+		if (aZ > 0) {
+		aData.set     (aX+ 8,  7, aZ+ 7, SIDE_Z_POS, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_N | SBIT_S, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		for (int tZ = aZ- 7, eZ = aZ+ 6; tZ <= eZ; tZ++)
+		aData.set     (aX+ 8,  7, tZ   , SIDE_Z_NEG, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_N | SBIT_S, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		}
+		if (aZ < 0) {
+		aData.set     (aX+ 8,  7, aZ+ 9, SIDE_Z_NEG, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_N | SBIT_S, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		for (int tZ = aZ+10, eZ = aZ+23; tZ <= eZ; tZ++)
+		aData.set     (aX+ 8,  7, tZ   , SIDE_Z_POS, 25377, UT.NBT.make(NBT_CONNECTION, SBIT_N | SBIT_S, NBT_COLOR, DYES_INT[aData.mColor], NBT_PAINTED, T), T, T);
+		}
+		
+		// Water Placement in center Area.
+		aData.smooth(aX+ 1,  9, aZ+ 1); aData.smooth(aX+ 2,  9, aZ+ 1); aData.smooth(aX+ 3,  9, aZ+ 1); aData.smooth(aX+ 4,  9, aZ+ 1);
+		aData.smooth(aX+ 1,  9, aZ+ 2); aData.smooth(aX+ 2,  9, aZ+ 2); aData.smooth(aX+ 3,  9, aZ+ 2);
+		aData.smooth(aX+ 1,  9, aZ+ 3); aData.smooth(aX+ 2,  9, aZ+ 3);
+		aData.smooth(aX+ 1,  9, aZ+ 4);
+		aData.smooth(aX+14,  9, aZ+ 1); aData.smooth(aX+13,  9, aZ+ 1); aData.smooth(aX+12,  9, aZ+ 1); aData.smooth(aX+11,  9, aZ+ 1);
+		aData.smooth(aX+14,  9, aZ+ 2); aData.smooth(aX+13,  9, aZ+ 2); aData.smooth(aX+12,  9, aZ+ 2);
+		aData.smooth(aX+14,  9, aZ+ 3); aData.smooth(aX+13,  9, aZ+ 3);
+		aData.smooth(aX+14,  9, aZ+ 4);
+		aData.smooth(aX+ 1,  9, aZ+14); aData.smooth(aX+ 2,  9, aZ+14); aData.smooth(aX+ 3,  9, aZ+14); aData.smooth(aX+ 4,  9, aZ+14);
+		aData.smooth(aX+ 1,  9, aZ+13); aData.smooth(aX+ 2,  9, aZ+13); aData.smooth(aX+ 3,  9, aZ+13);
+		aData.smooth(aX+ 1,  9, aZ+12); aData.smooth(aX+ 2,  9, aZ+12);
+		aData.smooth(aX+ 1,  9, aZ+11);
+		aData.smooth(aX+14,  9, aZ+14); aData.smooth(aX+13,  9, aZ+14); aData.smooth(aX+12,  9, aZ+14); aData.smooth(aX+11,  9, aZ+14);
+		aData.smooth(aX+14,  9, aZ+13); aData.smooth(aX+13,  9, aZ+13); aData.smooth(aX+12,  9, aZ+13);
+		aData.smooth(aX+14,  9, aZ+12); aData.smooth(aX+13,  9, aZ+12);
+		aData.smooth(aX+14,  9, aZ+11);
+		aData.set(aX+ 1, 10, aZ+ 1, Blocks.flowing_water, 0, 3);
+		aData.set(aX+ 1, 10, aZ+14, Blocks.flowing_water, 0, 3);
+		aData.set(aX+14, 10, aZ+ 1, Blocks.flowing_water, 0, 3);
+		aData.set(aX+14, 10, aZ+14, Blocks.flowing_water, 0, 3);
 		
 		return T;
 	}
