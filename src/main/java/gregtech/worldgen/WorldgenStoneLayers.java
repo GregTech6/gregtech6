6f64d02c2d997e7d7fcd1a47cd32881d7c86a41c
Gregorius Techneticies <gregorius.techneticies@protonmail.ch>
2025-05-06 12:25:59 +0200

    noticed a village having their cobblestone replaced by black granite instead of andesite bricks

--- src/main/java/gregtech/worldgen/WorldgenStoneLayers.java
+++ src/main/java/gregtech/worldgen/WorldgenStoneLayers.java
@@ -141,6 +141,9 @@ public class WorldgenStoneLayers extends WorldgenObject {
 							aStorage.setExtBlockMetadata(i, tY & 15, j, tScan[3].mMetaMossy);
 						}
 					}
+				// Check for the GT6 Stone being natural. Unlikely case due to GT6 Stone being the thing that is supposed to generate this very moment and not before. But Villages would otherwise see their House Materials replaed.
+				} else if (aBlock instanceof BlockStones) {
+					tCanPlaceRocks = (aStorage.getExtBlockMetadata(i, tY & 15, j) < 3);
 				// Stone and Ore Generation in replaceable Blocks.
 				} else if (aBlock == tLastReplaced || StoneLayer.REPLACEABLE_BLOCKS.contains(aBlock)) {
 					tLastReplaced = aBlock;
@@ -171,9 +174,6 @@ public class WorldgenStoneLayers extends WorldgenObject {
 							aStorage.setExtBlockMetadata(i, tY & 15, j, tScan[3].mMetaStone);
 						}
 					}
-				// Check for the GT6 Stone being natural. Unlikely case due to GT6 Stone being the thing that is supposed to generate this very moment and not before.
-				} else if (aBlock instanceof BlockStones) {
-					tCanPlaceRocks = (aStorage.getExtBlockMetadata(i, tY & 15, j) < 3);
 				// Place Rock if on Opaque Surface.
 				} else if (WD.easyRep(aWorld, tX, tY, tZ, aBlock)) {
 					if (tCanPlaceRocks && !aBlock.getMaterial().isLiquid() && aRandom.nextInt(128) == 0) tRegistry.mBlock.placeBlock(aWorld, tX, tY, tZ, SIDE_UNKNOWN, (short)32757, ST.save(NBT_VALUE, OP.rockGt.mat(aRandom.nextBoolean()&&tLastOre!=null?tLastOre.mTargetCrushing.mMaterial:tLastRock, 1)), F, T);
