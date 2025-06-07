20b27602dab614cf2410d4ed1150392203700aea
Mr-CHENmatrix <2391469284@qq.com>
2025-06-07 23:07:38 +0800

    now Tar can use to fuel

--- src/main/java/gregtech/loaders/b/Loader_Fuels.java
+++ src/main/java/gregtech/loaders/b/Loader_Fuels.java
@@ -71,6 +71,8 @@ public class Loader_Fuels implements Runnable {
 		
 		FM.Burn         .addRecipe0(T, -  4,  1, FL.Glue.make(1)                             , FL.CarbonDioxide.make(1), ZL_IS);
 		
+		FM.Burn         .addRecipe0(T, -  32,  4, FL.Tar.make(1)                             , FL.CarbonDioxide.make(1), ZL_IS);
+		
 		if (FL.JetFuel.exists()) {
 		FM.Burn         .addRecipe0(T, -128,  9, FL.JetFuel.make(1)                          , FL.CarbonDioxide.make(1), ZL_IS);
 		FM.Engine       .addRecipe0(T, -128, 12, FL.JetFuel.make(1)                          , FL.CarbonDioxide.make(1), ZL_IS);
